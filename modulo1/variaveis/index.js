46be7cf33fc39c030769f97891d2e2734ba55161
Ederson Braga <braga.edermello@gmail.com>
2021-10-04 16:57:00 -0300

    exercicios-variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,43 @@
+//Exercício de Interpretação de código
+
+
+//RESPOSTA 1°: 
+//10 
+//10 5
+
+////RESPOSTA 2°:
+//10 10 10
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert ("Você recebe", {t,p}, "por hora")
+
+//Exercícios de escrita de código 1.Construa um programa, seguindo os seguintes passos
+let nome = ("Ederson")
+let idade = ("27")
+console.log(nome)
+console.log(idade)
+//Porque usei o typeof que imprimir o resultado como String
+
+let nomeDoUsuario = prompt ("Qual é seu nome?")
+let idadeDoUsuario = prompt ("Qual sua idade?")
+// console.log(typeof nomeDoUsuario)
+// console.log(typeof idadeDoUsuario)
+//Imprimiu resultado como string
+console.log("Olá", nomeDoUsuario, "você tem", idadeDoUsuario, "anos!")
+
+//Escreva um programa que faça 3 perguntas...
+let corPreferida = prompt ("Qual é sua cor preferida?")
+let comidaPreferida = prompt ("Qual é sua comida preferida?")
+let sobremesaPreferida = prompt ("Qual é sua sobremesa preferida?")
+console.log("Qual é sua cor preferida?", corPreferida)
+console.log("Qual é sua comida preferida?", comidaPreferida)
+console.log("Qual é sua sobremesa preferida?", sobremesaPreferida)
+
+let a = 10
+let b = 25
+console.log("O valor de a é", a)
+console.log("O valor de b é", b)
+
+console.log("O novo valor de a é", b)
+console.log("O novo valor de b é", a)
