a533820f3583dbb913153dd2a7fa929e7aaba20e
Ederson Braga <braga.edermello@gmail.com>
2021-10-06 17:24:39 -0300

    Atividade-strings-arrays

--- modulo1/operadores/index.js
+++ modulo1/operadores/index.js
@@ -26,33 +26,49 @@ console.log("Resultado:", soma)
  
 /* Exercício 1
 */
-const suaIdade = prompt("Qual é a sua idade?")
-const idadeAmigo = prompt("Qual é a idade de seu melhor amigo?")
+// const suaIdade = prompt("Qual é a sua idade?")
+// const idadeAmigo = prompt("Qual é a idade de seu melhor amigo?")
 
-console.log("Sua idade é maior do que a do seu melhor amigo:", suaIdade > idadeAmigo)
-console.log("A diferença de idade é de:", suaIdade % idadeAmigo, "anos.")
+// console.log("Sua idade é maior do que a do seu melhor amigo:", suaIdade > idadeAmigo)
+// console.log("A diferença de idade é de:", suaIdade % idadeAmigo, "anos.")
 
-/* Exercício 2
-*/
-const numeroPar = prompt("Insira um número par:")
-console.log(numeroPar % 2)
+// /* Exercício 2
+// */
+// const numeroPar = prompt("Insira um número par:")
+// console.log(numeroPar % 2)
 // c) todos os numeros pares colocados sempre retornarão por usar resto da divisão em 0
 // d) todos os numeros impares colocados sempre retornarão por usar resto da divisão em 1
 
 /* Exercício 3
 */
-const suaIdade = prompt("Qual é a sua idade?")
+// const suaIdade = prompt("Qual é a sua idade?")
 
-console.log("Idade em meses:", suaIdade * 12)
-console.log("Idade em dias:", suaIdade * 365)
-console.log("Idade em horas:", suaIdade * 24)
+// console.log("Idade em meses:", suaIdade * 12)
+// console.log("Idade em dias:", suaIdade * 365)
+// console.log("Idade em horas:", suaIdade * 24)
 
-/* Exercício 4
-*/
-const numeroPrimeiro = prompt("Digite o primeiro número:")
-const numeroSegundo = prompt("Digite o segundo número:")
+// /* Exercício 4
+// */
+// const numeroPrimeiro = prompt("Digite o primeiro número:")
+// const numeroSegundo = prompt("Digite o segundo número:")
 
-console.log("O primeiro numero é maior que segundo?", numeroPrimeiro > numeroSegundo)
-console.log("O primeiro numero é igual ao segundo?", numeroPrimeiro === numeroSegundo)
-console.log("O primeiro numero é divisível pelo segundo?", numeroPrimeiro % numeroSegundo !== 0)
-console.log("O segundo numero é divisível pelo primeiro?", numeroSegundo % numeroPrimeiro !== 0)
+// console.log("O primeiro numero é maior que segundo?", numeroPrimeiro > numeroSegundo)
+// console.log("O primeiro numero é igual ao segundo?", numeroPrimeiro === numeroSegundo)
+// console.log("O primeiro numero é divisível pelo segundo?", numeroPrimeiro % numeroSegundo !== 0)
+// console.log("O segundo numero é divisível pelo primeiro?", numeroSegundo % numeroPrimeiro !== 0)
+
+const nome = "Chijo"
+const nomeAlterado = nome.toLowerCase()
+
+console.log(nomeAlterado)
+
+// const frase = "Hoje assisti Chaves, adoro Chaves!"
+// const novaFrase = frase.repleceAll("Chaves", "Tarzan")
+
+// console.log(novaFrase)
+
+
+const instrutoras = ["Amanda", "Lais"]
+instrutoras.push("Chijo")
+
+console.log(instrutoras)
