acce76406922f26cb42790d798a05df4f4477c78
ederbramello <braga.edermello@gmail.com>
2021-12-12 19:00:41 -0300

    implementação do codigo

--- modulo3/projeto-labex/labex/src/App.js
+++ modulo3/projeto-labex/labex/src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import HomePage from './pages/HomePage'
+import ListTripsPage from './pages/ListTripsPage';
+import ApplicationFormPage from './pages/ApplicationFormPage';
+import LoginPage from './pages/LoginPage';
+import AdminHomePage from './pages/AdminHomePage';
+import CreateTripPage from './pages/CreateTripPage';
+import TripDetailsPage from './pages/TripDetailsPage';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import axios from 'axios';
+import {useHistory} from "react-router-dom"
 
 function App() {
+
+  const [viagens, setviagens] = useState ([])
+
+  const history = useHistory()
+
+  const getTrips = () => {
+
+    axios.get ('https://us-central1-labenu-apis.cloudfunctions.net/labeX/ederson-mello-carver/trips')
+
+    .then ((res) => {
+      setviagens (res.data.trips)
+
+    })
+    .catch ((err) => {
+      alert (err.data)
+    })
+  }
+
+  useEffect(() => {
+
+    getTrips()
+
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+
+      <BrowserRouter>
+        <Switch>
+
+          <Route exact path={"/"}>
+            <HomePage />
+          </Route>
+
+          <Route exact path={"/trips/list"}>
+            <ListTripsPage viagens={viagens}/>
+          </Route>
+
+          <Route exact path={"/trips/application"}>
+            <ApplicationFormPage  viagens={viagens} getTrips={getTrips}/>
+          </Route>
+
+          <Route exact path={"/login"}>
+            <LoginPage />
+          </Route>
+
+          <Route exact path={"/admin/trips/list"}>
+            <AdminHomePage viagens={viagens} getTrips={getTrips} />
+          </Route>
+
+          <Route exact path={"/admin/trips/create"}>
+            <CreateTripPage getTrips={getTrips}/>
+          </Route>
+
+          <Route exact path={"/admin/trips/:id"}>
+            <TripDetailsPage />
+          </Route>
+
+        </Switch>
+
+      </BrowserRouter>
+
     </div>
   );
 }
